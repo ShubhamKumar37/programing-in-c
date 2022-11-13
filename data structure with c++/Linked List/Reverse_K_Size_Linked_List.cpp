ef8f3c742a527e93816db81d07f85653f3eee237
ShubhamKumar37 <107699951+ShubhamKumar37@users.noreply.github.com>
2022-11-13 21:22:48 +0530

    shubham

--- data structure with c++/Linked List/Reverse_K_Size_Linked_List.cpp
+++ data structure with c++/Linked List/Reverse_K_Size_Linked_List.cpp
@@ -1,33 +1,33 @@
 
 class Solution
 {
-    public:
-    struct node *reverse (struct node *Head, int K)
-    { 
+public:
+    struct node *reverse(struct node *Head, int K)
+    {
         // Complete this method
-        if(Head==NULL)
+        if (Head == NULL)
         {
             return NULL;
         }
-        
-        node* Prev=NULL;
-        node* Curr=Head;
-        node* Forward=NULL;
-        int C=0;
-        while(C<K && Curr!=NULL)
+
+        node *Prev = NULL;
+        node *Curr = Head;
+        node *Forward = NULL;
+        int C = 0;
+        while (C < K && Curr != NULL)
         {
-            Forward=Curr->next;
-            Curr->next=Prev;
-            Prev=Curr;
-            Curr=Forward;
+            Forward = Curr->next;
+            Curr->next = Prev;
+            Prev = Curr;
+            Curr = Forward;
             C++;
         }
-        
-        if(Forward!=NULL)
+
+        if (Forward != NULL)
         {
-            Head->next=reverse(Forward,K);
+            Head->next = reverse(Forward, K);
         }
-        
+
         return Prev;
     }
 };

fb79a710675c4b7b2437b2ee9314f9f9497a162b
ShubhamKumar37 <107699951+ShubhamKumar37@users.noreply.github.com>
2022-11-13 15:46:26 +0530

    shubham

---
+++ data structure with c++/Linked List/Middle_Element_Linked_List.cpp
@@ -0,0 +1,31 @@
+struct Node {
+    int data;
+    Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+    
+}; */
+class Solution{
+    public:
+    /* Should return data of middle node. If linked list is empty, then  -1*/
+    int getMiddle(Node *Head)
+    {
+        // Your code here}
+        if(Head->next==NULL)
+        {
+            return Head->data;
+        }
+        Node* Noob=Head;
+        Node* Pro=Head;
+        while(Pro!=NULL && Pro->next!=NULL)
+        {
+            Pro=Pro->next->next;
+            Noob=Noob->next;
+        }
+        return Noob->data;
+        
+    }
+};
---
+++ data structure with c++/Linked List/Reverse_Linked_List.cpp
@@ -0,0 +1,59 @@
+struct Node
+{
+    int data;
+    struct Node *next;
+}
+
+*/
+
+class Solution
+{
+    private:
+    void RVL_Rec(Node* &Head,Node* Prev,Node* Curr)
+    {
+        if(Curr==NULL)
+        {
+            Head=Prev;
+            return ;
+        }
+        Node* Forward=Curr->next;
+        RVL_Rec(Head,Curr,Forward);
+        Curr->next=Prev;
+    }
+    public:
+    //Function to reverse a linked list.
+    struct Node* reverseList(struct Node *Head)
+    {
+        // code here
+        // return head of reversed list
+        Node* Prev=NULL;
+        Node* Curr=Head;
+        RVL_Rec(Head,Prev,Curr);
+        return Head;
+
+
+
+        // if(Head==NULL || Head->next==NULL)
+        // {
+        //     return Head;
+        // }
+        // Node* Prev=NULL;
+        // Node* Curr=Head;
+        // Node* Forward=NULL;
+        
+        // while(Curr!=NULL)
+        // {
+        //     Forward=Curr->next;
+        //     Curr->next=Prev;
+        //     Prev=Curr;
+        //     Curr=Forward;
+        // }
+        
+        // return Prev;
+        
+        
+        
+        
+    }
+    
+};

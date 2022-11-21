a2ca0d0416216dae2fdfe2b58982be6ae5bc7258
ShubhamKumar37 <107699951+ShubhamKumar37@users.noreply.github.com>
2022-11-22 02:35:12 +0530

    shubham

---
+++ data structure with c++/Linked List/Delete_Current_Node_Smaller_Right_Side_Linked_List.cpp
@@ -0,0 +1,73 @@
+The structure of linked list is the following
+
+struct Node
+{
+    int data;
+    Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+*/
+class Solution
+{
+    private:
+    Node* Reverse(Node* Head)
+    {
+        Node* Curr=Head;
+        Node* Prev=NULL;
+        Node* Forward=NULL;
+        
+        while(Curr!=NULL)
+        {
+            Forward=Curr->next;
+            Curr->next=Prev;
+            Prev=Curr;
+            Curr=Forward;
+        }
+        
+        return Prev;
+    }
+    
+    public:
+    Node *compute(Node *Head)
+    {
+        // your code goes here
+        if(Head->next==NULL)
+        {
+            return Head;
+        }
+        
+        Head=Reverse(Head);
+        int Max_Val=INT_MIN;
+        Node* Curr=Head;
+        Node* Prev=NULL;
+        Node* Forward=NULL;
+        
+        while(Curr!=NULL)
+        {
+            
+            Forward=Curr->next;
+            if(Curr->data>=Max_Val)// Just update the value of Max_Val
+            {
+                Max_Val=Curr->data;
+                Prev=Curr;
+                Curr=Forward;
+            }
+            else // Deleted the node which is smaller then Max_Val
+            {
+                Node* T_Del=Curr;
+                Prev->next=Forward;
+                Curr=Forward;
+                T_Del->next=NULL;
+                free(T_Del);
+            }
+        }
+        
+        return Reverse(Head); // Reverse the list again
+        
+    }
+    
+};

25c84ea5228c4d02abc7bbfaee5d6e68bfd76f21
ShubhamKumar37 <107699951+ShubhamKumar37@users.noreply.github.com>
2022-11-27 00:14:08 +0530

    shubham

--- data structure with c++/Linked List/Delete_Current_Node_Smaller_Right_Side_Linked_List.cpp
+++ data structure with c++/Linked List/Delete_Current_Node_Smaller_Right_Side_Linked_List.cpp
@@ -1,73 +1,72 @@
 The structure of linked list is the following
 
-struct Node
+    struct Node
 {
     int data;
-    Node* next;
-    
-    Node(int x){
+    Node *next;
+
+    Node(int x)
+    {
         data = x;
         next = NULL;
     }
 };
-*/
-class Solution
+* /
+    class Solution
 {
-    private:
-    Node* Reverse(Node* Head)
+private:
+    Node *Reverse(Node *Head)
     {
-        Node* Curr=Head;
-        Node* Prev=NULL;
-        Node* Forward=NULL;
-        
-        while(Curr!=NULL)
+        Node *Curr = Head;
+        Node *Prev = NULL;
+        Node *Forward = NULL;
+
+        while (Curr != NULL)
         {
-            Forward=Curr->next;
-            Curr->next=Prev;
-            Prev=Curr;
-            Curr=Forward;
+            Forward = Curr->next;
+            Curr->next = Prev;
+            Prev = Curr;
+            Curr = Forward;
         }
-        
+
         return Prev;
     }
-    
-    public:
+
+public:
     Node *compute(Node *Head)
     {
         // your code goes here
-        if(Head->next==NULL)
+        if (Head->next == NULL)
         {
             return Head;
         }
-        
-        Head=Reverse(Head);
-        int Max_Val=INT_MIN;
-        Node* Curr=Head;
-        Node* Prev=NULL;
-        Node* Forward=NULL;
-        
-        while(Curr!=NULL)
+
+        Head = Reverse(Head);
+        int Max_Val = INT_MIN;
+        Node *Curr = Head;
+        Node *Prev = NULL;
+        Node *Forward = NULL;
+
+        while (Curr != NULL)
         {
-            
-            Forward=Curr->next;
-            if(Curr->data>=Max_Val)// Just update the value of Max_Val
+
+            Forward = Curr->next;
+            if (Curr->data >= Max_Val) // Just update the value of Max_Val
             {
-                Max_Val=Curr->data;
-                Prev=Curr;
-                Curr=Forward;
+                Max_Val = Curr->data;
+                Prev = Curr;
+                Curr = Forward;
             }
             else // Deleted the node which is smaller then Max_Val
             {
-                Node* T_Del=Curr;
-                Prev->next=Forward;
-                Curr=Forward;
-                T_Del->next=NULL;
+                Node *T_Del = Curr;
+                Prev->next = Forward;
+                Curr = Forward;
+                T_Del->next = NULL;
                 free(T_Del);
             }
         }
-        
+
         return Reverse(Head); // Reverse the list again
-        
     }
-    
 };

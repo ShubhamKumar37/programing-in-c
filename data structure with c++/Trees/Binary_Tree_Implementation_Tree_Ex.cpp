6906f2e0b2d81bd61544c949b174624fc99840c5
ShubhamKumar37 <107699951+ShubhamKumar37@users.noreply.github.com>
2022-11-27 13:51:07 +0530

    shubham

--- data structure with c++/Trees/Binary_Tree_Implementation_Tree_Ex.cpp
+++ data structure with c++/Trees/Binary_Tree_Implementation_Tree_Ex.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <QUEUE>
 
 using namespace std;
 
@@ -16,34 +17,130 @@ public:
     }
 };
 
-void Creation_Tree(Node *& Root)
+Node *Creation_Tree(Node *Root)
 {
     cout << "Enter the data = ";
     int Data;
     cin >> Data;
-    Root=new Node(Data);
+    Root = new Node(Data);
 
     if (Data == -1)
     {
-        return ;
+        return NULL;
     }
 
-    cout << "Enter the data for left skewed !" << endl;
-    // Root->left = Creation_Tree(Root->left);
-    Creation_Tree(Root->left);
+    cout << "Enter the data for left skewed of !" << Data << endl;
+    Root->left = Creation_Tree(Root->left);
 
-    cout << "Enter the data for right skewed !" << endl;
-    Creation_Tree(Root->right);
-    // Root->right = Creation_Tree(Root->right);
+    cout << "Enter the data for right skewed of !" << Data << endl;
+    Root->right = Creation_Tree(Root->right);
 
-    return ;
+    return Root;
+}
+
+void Traversal_Level_Order(Node *Root)
+{
+    queue<Node *> Ans;
+    Ans.push(Root);
+    Ans.push(NULL);
+    while (!Ans.empty())
+    {
+        Node *Temp = Ans.front();
+        cout << Temp->data << " ";
+        Ans.pop();
+
+        if (Temp == NULL)
+        {
+            cout << endl;
+            if (!Ans.empty())
+            {
+                Ans.push(NULL);
+            }
+        }
+        else
+        {
+
+            if (Temp->left )
+            {
+                Ans.push(Temp->left);
+            }
+            if (Temp->right )
+            {
+                Ans.push(Temp->right);
+            }
+        }
+    }
+    cout<<endl;
+}
+
+void Pre_Traversal(Node* Root)
+{
+    if(Root==NULL)
+    {
+        return ;
+    }
+    cout<<Root->data<<" ";
+    Pre_Traversal(Root->left);
+    Pre_Traversal(Root->right);
+}
+void In_Traversal(Node* Root)
+{
+    if(Root==NULL)
+    {
+        return ;
+    }
+    In_Traversal(Root->left);
+    cout<<Root->data<<" ";
+    In_Traversal(Root->right);
+}
+void Post_Traversal(Node* Root)
+{
+    if(Root==NULL)
+    {
+        return ;
+    }
+    Post_Traversal(Root->left);
+    Post_Traversal(Root->right);
+    cout<<Root->data<<" ";
+}
+
+Node* Create_Node(int Data)
+{
+    Node* Root;
+    Root->data=Data;
+    Root->left=NULL;
+    Root->right=NULL;
+
+    return Root;
 }
 
 int main()
 {
-    Node *Root = NULL;
 
-    Creation_Tree(Root);
+    Node* Root=NULL;
+    Root=Creation_Tree(Root);
+    cout<<endl;
+    Pre_Traversal(Root);
+    cout<<endl;
+    In_Traversal(Root);
+    cout<<endl;
+    Post_Traversal(Root);
+
+    // Node *Root= Create_Node(1);
+    // Node *L1 = Create_Node(2);
+    // Node *R1 = Create_Node(3);
+    // Node *L1_1 = Create_Node(4);
+    // Node *R1_1 = Create_Node(5);
+    // Root->left=L1;
+    // Root->right=R1;
+    // L1->left=L1_1;
+    // R1->left=R1_1;
+    // cout<<"Done"<<endl;
+
+    // Root = Creation_Tree(Root);
+    // cout<<"Value in root = "<<Root->left->left->data<<endl;
+    // Traversal_Level_Order(Root);
+
 
     return 0;
 }

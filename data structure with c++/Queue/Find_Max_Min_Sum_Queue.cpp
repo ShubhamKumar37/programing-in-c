bbda1ca7a8bfa9d6a6e1bd8e6c4a54e8b2392cde
ShubhamKumar37 <107699951+ShubhamKumar37@users.noreply.github.com>
2022-12-21 01:50:22 +0530

    shubham

--- data structure with c++/Queue/Find_Max_Min_Sum_Queue.cpp
+++ data structure with c++/Queue/Find_Max_Min_Sum_Queue.cpp
@@ -1,23 +1,23 @@
-#include<iostream>
-#include<deque>
+#include <iostream>
+#include <deque>
 
 using namespace std;
 
-int Solve(int *Arr ,int K ,int N)
+int Solve(int *Arr, int K, int N)
 {
-    deque <int> Min(K);
-    deque <int> Max(K);
+    deque<int> Min(K);
+    deque<int> Max(K);
     int Sum = 0;
 
-    for(int i=0; i<K; i++)
+    for (int i = 0; i < K; i++)
     {
-        while(!Min.empty() && Arr[Min.front()] >= Arr[i])
+        while (!Min.empty() && Arr[Min.back()] >= Arr[i])
         {
-            Min.pop_front();
+            Min.pop_back();
         }
-        while(!Max.empty() && Arr[Max.front()] <= Arr[i])
+        while (!Max.empty() && Arr[Max.back()] <= Arr[i])
         {
-            Max.pop_front();
+            Max.pop_back();
         }
         Max.push_back(i);
         Min.push_back(i);
@@ -25,15 +25,23 @@ int Solve(int *Arr ,int K ,int N)
 
     Sum = Arr[Max.front()] + Arr[Min.front()];
 
-    for(int i=K; i<N; i++)
+    for (int i = K; i < N; i++)
     {
-        while(!Min.empty() && Arr[Min.front()] >= Arr[i])
+        if(!Min.empty() && i - Min.front() >= K)
         {
             Min.pop_front();
         }
-        while(!Max.empty() && Arr[Max.front()] <= Arr[i])
+        if(!Max.empty() && i - Max.front() >= K)
         {
             Max.pop_front();
+        }
+        while (!Min.empty() && Arr[Min.back()] >= Arr[i])
+        {
+            Min.pop_back();
+        }
+        while (!Max.empty() && Arr[Max.back()] <= Arr[i])
+        {
+            Max.pop_back();
         }
         Max.push_back(i);
         Min.push_back(i);
@@ -43,10 +51,41 @@ int Solve(int *Arr ,int K ,int N)
     return Sum;
 }
 
+int Alt_Solve(int *Arr, int K, int N)
+{
+    int P_Min = INT_MAX;
+    int P_Max = INT_MIN;
+    int Min = Arr[0];
+    int Max = Arr[0];
+    int Ans = 0;
+
+    int i = 0 , j = 1;
+    while(j < N)
+    {
+        if(j < K)
+        {
+            P_Min = max(Min , Arr[j]) == Min ? Min : P_Min ;
+            P_Max = min(Max , Arr[j]) == Max ? Max : P_Max ;
+            Min = min(Arr[j] , Min);
+            Max = max(Arr[j] , Max);
+            j++;
+        }
+        else if(j - i + 1 == K)
+        {
+            Ans = Min + Max;
+            P_Min = max(Min , Arr[j]) == Min ? Min : P_Min ;
+            P_Max = min(Max , Arr[j]) == Max ? Max : P_Max ;
+            Min = min(Arr[j] , Min);
+            Max = max(Arr[j] , Max);
+        }
+    }
+}
+
 int main()
 {
-    int Arr[] = {2, 5, -1, 7, -3, -1, -2}  ;
-    int K = 4;
+    // int Arr[] = {2, 5, -1, 7, -3, -1, -2};
+    int Arr[] = {1,2,3,4,5,6};
+    int K = 3;
     int N = sizeof(Arr) / sizeof(int);
     cout << "Ans of the question is " << Solve(Arr, K, N);
 

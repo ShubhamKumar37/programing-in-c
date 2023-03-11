fc3a451ed8f9dad85fcc503a9f10dcb7f980731e
ShubhamKumar37 <107699951+ShubhamKumar37@users.noreply.github.com>
2023-03-12 00:42:48 +0530

    shubham

---
+++ data structure with c++/Trees/Binary_Tree_Implementation.cpp
@@ -0,0 +1,139 @@
+#include <iostream>
+#include <QUEUE>
+
+using namespace std;
+
+class Root
+{
+public:
+    int Data;
+    Root *Left;
+    Root *Right;
+
+    Root(int Data)
+    {
+        this->Data = Data;
+        this->Left = NULL;
+        this->Right = NULL;
+    }
+};
+
+Root *Build_Binary_Tree(Root *R);
+void Level_Order_Traversal(Root *R);
+void Pre_Oreder_Traversal(Root *R);
+void In_Oreder_Traversal(Root *R);
+void Post_Oreder_Traversal(Root *R);
+
+// 1 3 7 -1 -1 11 -1 -1 5 17 -1 -1 -1
+int main()
+{
+    Root *R = NULL;
+
+    R = Build_Binary_Tree(R);
+    cout << endl;
+    Level_Order_Traversal(R);
+    cout << endl;
+    cout << "Preordered traversal = " << endl;
+    Pre_Oreder_Traversal(R);
+    // cout << endl;
+    // In_Oreder_Traversal(R);
+    // cout << endl;
+    // Post_Oreder_Traversal(R);
+
+    return 0;
+}
+
+void Pre_Oreder_Traversal(Root *R)
+{
+    if(R == NULL)
+    {
+        return ;
+    }
+    Root *Temp = R;
+    cout << Temp -> Data << " ";
+    Level_Order_Traversal(Temp -> Left);
+    Level_Order_Traversal(Temp -> Right);
+}
+void In_Oreder_Traversal(Root *R)
+{
+    if(R == NULL)
+    {
+        return ;
+    }
+    Root *Temp = R;
+    Level_Order_Traversal(Temp -> Left);
+    cout << Temp -> Data << " ";
+    Level_Order_Traversal(Temp -> Right);
+}
+void Post_Oreder_Traversal(Root *R)
+{
+    if(R == NULL)
+    {
+        return ;
+    }
+    Root *Temp = R;
+    Level_Order_Traversal(Temp -> Left);
+    Level_Order_Traversal(Temp -> Right);
+    cout << Temp -> Data << " ";
+}
+
+void Level_Order_Traversal(Root *R) 
+{
+    if(R == NULL)
+    {
+        cout << "Tree is empty!!" << endl;
+        return ;
+    }
+
+    queue<Root *> Q;
+    Q.push(R);
+    Q.push(NULL);
+
+    while(!Q.empty())
+    {
+        Root *Temp = Q.front();
+        Q.pop();
+
+        if(Temp == NULL)
+        {
+            cout << endl;
+            if(!Q.empty())
+            {
+                Q.push(NULL);
+            }
+        }
+        else
+        {
+            cout << Temp -> Data << " ";
+            if(Temp -> Left != NULL)
+            {
+                Q.push(Temp -> Left);
+            }
+            if(Temp -> Right != NULL)
+            {
+                Q.push(Temp -> Right);
+            }
+        }
+    }
+}
+
+Root *Build_Binary_Tree(Root *R)
+{
+    int Data;
+    cout << "Enter the data for Node = ";
+    cin >> Data;
+    Root *Node = new Root(Data);
+
+    if (Data == -1)
+    {
+        return NULL;
+    }
+
+    cout << "Enter the data for left skewed of " << Data << endl;
+    Node->Left = Build_Binary_Tree(Node);
+
+    cout << "Enter the data for right skewed of " << Data << endl;
+    Node->Right = Build_Binary_Tree(Node);
+
+    return Node;
+}

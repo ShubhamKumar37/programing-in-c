3d3c2d13dc1cfd935cb4c484b5864a62132cea65
ShubhamKumar37 <107699951+ShubhamKumar37@users.noreply.github.com>
2023-05-23 14:06:51 +0530

    shubham

--- data structure with c++/Tries/Trie_Implementation.cpp
+++ data structure with c++/Tries/Trie_Implementation.cpp
@@ -1,27 +1,37 @@
-#include<iostream>
+#include <iostream>
+#include <stack>
 
 using namespace std;
 
 class Trie_Node
 {
-    public:
+public:
     char data;
-    Trie_Node* Children[26];
+    Trie_Node *Children[26];
     bool Is_Reached = false;
 
     Trie_Node(char data)
     {
-        this -> data = data;
-        for(int i = 0; i < 26; i++)
+        this->data = data;
+        for (int i = 0; i < 26; i++)
         {
             Children[i] = NULL;
         }
+    }
+
+    ~Trie_Node()
+    {
+        for (int i = 0; i < 26; i++)
+        {
+            delete Children[i];
+        }
+        cout << "Memory deallocated" << endl;
     }
 };
 
 class Trie
 {
-    public:
+public:
     Trie_Node *root;
     Trie(void)
     {
@@ -32,18 +42,19 @@ class Trie
     {
         Trie_Node *Child = root;
 
-        for(char i : Word)
+        for (char i : Word)
         {
             int Ind = i - 'a';
-            if(Child -> Children[Ind] == NULL)
+            if (Child->Children[Ind] == NULL)
             {
-                Child -> Children[Ind] = new Trie_Node(i);
+                Child->Children[Ind] = new Trie_Node(i);
             }
 
-            Child = Child -> Children[Ind];
+            Child = Child->Children[Ind];
         }
 
-        Child -> Is_Reached = true;
+        Child->Is_Reached = true;
+
         // if(Word.length() == 0)
         // {
         //     root -> Is_Reached = true;
@@ -74,22 +85,23 @@ class Trie
     {
         Trie_Node *Child = root;
 
-        for(char i : Word)
+        for (char i : Word)
         {
             int Ind = i - 'a';
-            if(Child -> Children[Ind] == NULL)
+            if (Child->Children[Ind] == NULL)
             {
                 return false;
             }
-            Child = Child -> Children[Ind];
+            Child = Child->Children[Ind];
         }
 
-        return true;
+        return Child->Is_Reached;
+
         // if(Word.length() == 0)
         // {
         //     return root -> Is_Reached;
         // }
-        
+
         // int Ind = Word[0] - 'a';
         // Trie_Node *Child;
 
@@ -97,7 +109,7 @@ class Trie
         // {
         //     Child = root -> Children[Ind];
         // }
-        // else 
+        // else
         // {
         //     return false;
         // }
@@ -109,33 +121,90 @@ class Trie
         return Search(root, Word);
     }
 
-    // void Remove_Word(Trie_Node *root, string Word)
-    // {
-    //     if(Word.length() == 0)
-    //     {
-    //         if(root -> Is_Reached)
-    //         {
-    //             root -> Is_Reached == false;
-    //             int Ind = root -> Children[]
-    //         }
-    //     }
-    // }
-    // void Remove(string Word)
-    // {
-    //     Remove_Word(root, Word);
-    // }
-};
+    bool Delete(Trie_Node *root, string Word)
+    {
+        Trie_Node *Child = root;
+        stack<Trie_Node *> S;
+        for (char i : Word)
+        {
+            int Ind = i - 'a';
+            if (Child->Children[Ind] == NULL)
+            {
+                return false;
+            }
+            Child = Child->Children[Ind];
+            S.push(Child);
+        }
 
+        if (Child->Is_Reached)
+        {
+            Child->Is_Reached = false;
+            while (!S.empty())
+            {
+                Trie_Node *Top = S.top();
+                S.pop();
+
+                int Ind = Word[S.size()] - 'a';
+                for (int i = 0; i < 26; i++)
+                {
+                    if (Top->Children[i] != NULL || Top->Is_Reached)
+                    {
+                        return false;
+                    }
+                }
+
+                if (S.size() > 1)
+                {
+                    S.top()->Children[Ind] = NULL;
+                    delete Top;
+                }
+            }
+            return true;
+        }
+        return false;
+    }
+    bool Delete_Word(string Word)
+    {
+        return Delete(root, Word);
+    }
+
+    void Traverse(Trie_Node *root, string Ans)
+    {
+        if (root == NULL)
+        {
+            return;
+        }
+
+        if (root->Is_Reached)
+        {
+            cout << Ans << endl;
+        }
+
+        for (int i = 0; i < 26; i++)
+        {
+            if (root->Children[i] != NULL)
+            {
+                char Str = 'a' + i;
+                Traverse(root->Children[i], Ans + Str);
+            }
+        }
+    }
+    void Traverse_Trie(void)
+    {
+        Traverse(root, "");
+    }
+};
 
 int main()
 {
 
     Trie *root = new Trie();
-    root -> Insert_Word("abcd");
-    root -> Insert_Word("abd");
-    root -> Insert_Word("abdc");
+    root->Insert_Word("abcd");
+    root->Insert_Word("abd");
+    root->Insert_Word("abdc");
+    root->Insert_Word("abdcfg");
 
-    bool Available = root->Search_Word("abdc");
+    bool Available = root->Search_Word("abdcfg");
 
     if (Available)
     {
@@ -145,5 +214,28 @@ int main()
     {
         cout << "String is not founded " << endl;
     }
+    root->Delete_Word("abdc");
+    Available = root->Search_Word("abdc");
+
+    if (Available)
+    {
+        cout << "String is founded " << endl;
+    }
+    else
+    {
+        cout << "String is not founded " << endl;
+    }
+
+    Available = root->Search_Word("abdc");
+    if (Available)
+    {
+        cout << "String is founded " << endl;
+    }
+    else
+    {
+        cout << "String is not founded " << endl;
+    }
+
+    root->Traverse_Trie();
     return 0;
 }

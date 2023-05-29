22cd2489be5840e83ec6957dbfdc23d6b6bad33c
ShubhamKumar37 <107699951+ShubhamKumar37@users.noreply.github.com>
2023-05-30 00:35:26 +0530

    shubham

--- data structure with c++/Graph/Graph_Implementation.cpp
+++ data structure with c++/Graph/Graph_Implementation.cpp
@@ -30,6 +30,103 @@ class Graph
             cout << endl;
         }
     }
+
+// void Make_Adjacent(vector<pair<int, int>> &edges, unordered_map<int, set<int>> &Adj)
+// {
+//     for(int i = 0; i < edges.size(); i++)
+//     {
+//         int v = edges[i].first;
+//         int u = edges[i].second;
+
+//         Adj[u].insert(v);
+//         Adj[v].insert(u);
+//     }
+// }
+
+    // void Traverse_BFS(unordered_map<int, set<int>> &Adj, unordered_map<int, bool> &Map, vector<int> &Ans, int Val)
+    // {
+    //     queue<int> Q;
+    //     Q.push(Val);
+    //     Map[Val] = true;
+    //     while(!Q.empty())
+    //     {
+    //         int Front = Q.front();
+    //         Q.pop();
+
+    //         Ans.push_back(Front);
+    //         for(auto i : Adj[Front])
+    //         {
+    //             if(Map[i] == false)
+    //             {
+    //                 Q.push(i);
+    //                 Map[i] = true;
+    //             }
+    //         }
+    //     }
+    // }
+
+    // vector<int> BFS(int vertex, vector<pair<int, int>> edges)
+    // {
+    //     // Write your code here
+    //     vector<int> Ans;
+    //     unordered_map<int, set<int>> Adj;
+    //     unordered_map<int, bool> Visited;
+
+    //     Make_Adjacent(edges, Adj);
+
+    //     for(int i = 0; i < vertex; i++)
+    //     {
+    //         if(!Visited[i])
+    //         {
+    //             Traverse_BFS(Adj, Visited, Ans, i);
+    //         }
+    //     }
+
+    //     return Ans;
+
+    // }
+
+    // void Make_DFS(unordered_map<int, set<int>> &Map, unordered_map<int, bool> &Visited, vector<int> &Temp, int Node)
+    // {
+    //     Visited[Node] = true;
+    //     Temp.push_back(Node);
+        
+    //     for(auto i : Map[Node])
+    //     {
+    //         if(!Visited[i])
+    //         {
+    //             Make_DFS(Map, Visited, Temp, i);
+    //         }
+    //     }
+    // }
+
+    // vector<vector<int>> depthFirstSearch(int V, int E, vector<vector<int>> &edges)
+    // {
+    //     // Write your code here
+    //     vector<vector<int>> Ans;
+    //     unordered_map<int, set<int>> Map;
+    //     unordered_map<int, bool> Visited;
+
+    //     for(int i = 0; i < E; i++)
+    //     {
+    //         Map[edges[i][0]].insert(edges[i][1]);
+    //         Map[edges[i][1]].insert(edges[i][0]);
+    //     }
+
+    //     for(int i = 0; i < V; i++)
+    //     {
+    //         if(!Visited[i])
+    //         {
+    //             vector<int> Temp;
+    //             Make_DFS(Map, Visited, Temp, i);
+    //             Ans.push_back(Temp);
+    //         }
+    //     }
+
+    //     return Ans;
+
+    // }
+
 };
 
 int main()

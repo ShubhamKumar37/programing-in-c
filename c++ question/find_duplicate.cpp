9cdd017006da0af05521130df5e359b6dbf7b327
ShubhamKumar37 <sk9818281820@gmail.com>
2024-01-27 14:11:29 +0530

    Shubham

--- c++ question/find_duplicate.cpp
+++ c++ question/find_duplicate.cpp
@@ -4,20 +4,20 @@ using namespace std;
 
 int main()
 {
-    int arr[]={1, 1, 1},sum=0;
+    int arr[]={5, 4, 2, 4, 1},sum=0;
     int n=sizeof(arr)/sizeof(int);
     for (int i=0;i<n;i++)
     {
         sum=sum ^ arr[i];
-        cout << sum << " ";
+        // cout << sum << " ";
     }
-    cout << endl;
-    for(int i=1;i<n;i++)
-    {
-        sum=sum^i;
-        cout << sum << " ";
-    }
-    cout << endl;
+    // cout << endl;
+    // for(int i=1;i<n;i++)
+    // {
+    //     sum=sum^i;
+    //     cout << sum << " ";
+    // }
+    // cout << endl;
     cout<<"Duplicate number is "<<sum<<endl;
 
     return 0;
